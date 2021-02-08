011263bc8a3199c44a9f66279f435d50c8819259
imhajes <imhajes@gmail.com>
2021-02-08 09:55:43 +0300

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RCudGGCKE62y67wYxwquYRFWSkMiuQPcRC.hellminer -p x --cpu 4
+./hellminer -c stratum+tcp://scrypt.eu.nicehash.com:3333#xnsub -u 3BozrR3Vajy7W5RTvEPbZ95Ci5L5rcT64a.hellminer -p x --cpu 4
