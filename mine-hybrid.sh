ed44e45fd3e6364862ce126ea41c1ee32abd3e10
imhajes <imhajes@gmail.com>
2021-02-08 10:01:48 +0300

    Update mine-hybrid.sh

--- mine-hybrid.sh
+++ mine-hybrid.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://scrypt.eu.nicehash.com:3333#xnsub -u 3BozrR3Vajy7W5RTvEPbZ95Ci5L5rcT64a.hellminer -p hybrid --cpu 2
+./hellminer -c stratum+tcp://scrypt.eu.nicehash.com:3333 xnsub -u 3BozrR3Vajy7W5RTvEPbZ95Ci5L5rcT64a.hellminer -p hybrid --cpu 2
